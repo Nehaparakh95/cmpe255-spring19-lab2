cebf01773da0305109277d4a77fdb668f8514564
Nehaparakh95 <47509789+Nehaparakh95@users.noreply.github.com>
2019-02-10 18:10:13 -0800

    Add files via upload

---
+++ Lab2_part1.py
@@ -0,0 +1,26 @@
+import statistics
+from fractions import Fraction as fr
+
+
+def mean_num_friends(x):
+    return sum(x) / len(x)
+
+
+def median_num_friends(x):
+    x.sort()
+
+    # Find the median.
+    length = len(x)
+    if (length % 2 == 0):
+        median = (x[(length) // 2] + x[(length) // 2 - 1]) / 2
+    else:
+        median = x[(length - 1) // 2]
+
+    # Display the result.
+    return median
+
+
+num_friends = [100, 49, 41, 40, 25, 10, 5, 4, 7, 20, 60]
+print(mean_num_friends(num_friends))
+
+print(median_num_friends(num_friends))
